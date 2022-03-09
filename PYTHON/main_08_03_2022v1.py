6c8732a5fe5bf3a949b3c2a2013b1d42be0162ac
Heigen007 <67007974+Heigen007@users.noreply.github.com>
2022-03-09 15:45:10 +0600

    Update main_08_03_2022v1.py

--- PYTHON/main_08_03_2022v1.py
+++ PYTHON/main_08_03_2022v1.py
@@ -10,7 +10,8 @@ globalTimeout = 20
 
 
 def ifnull(var):
-    if var is None: return str(var)
+    if var is None: return ""
+    if isinstance(var, int): return str(var)
     return var
 
 
@@ -99,8 +100,14 @@ def getRequestGetList(self, id):
             return generateErrorXml(self, "Сервер СУДМ 10.8.4.244:8010 не ответил на вызов GET /api/material-movement/v1.0/material-movement-request в течение " + str(globalTimeout) + " секунд",  False, 0)
         except Exception as e:
             return generateErrorXml(self, "Ошибка при вызове GET /api/material-movement/v1.0/material-movement-request (Сервер СУДМ 10.8.4.244:8010): " + str(e), False, 0)
-
+        
         json = r.json()
+
+        if(json is None or 'materialMovementRequests' not in json):
+            return generateErrorXml(self, "От сервера СУДМ 10.8.4.244:8010 на вызов GET /api/material-movement/v1.0/stock получен неожиданный ответ:", True, r.status_code)
+        elif(json['materialMovementRequests'] == []):
+            return generateErrorXml(self, "От сервера СУДМ 10.8.4.244:8010 на вызов GET-метода /api/material-movement/v1.0/stock получен пустой список materialMovementRequests",False, 0)
+
         xml= "<s:Envelope"
         xml+= "xmlns:s=\"http://schemas.xmlsoap.org/soap/envelope/\""
         xml+= "xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\""
@@ -111,16 +118,18 @@ def getRequestGetList(self, id):
         xml+= "xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\""
         xml+= "xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\">"
         for item in json['materialMovementRequests']:
-            t = item['createDate']
-            hours_added = datetime.timedelta(hours = int(t.split("+")[1].split(":")[0]) - 6, minutes = int(t.split("+")[1].split(":")[1]))
-            dateTime = datetime.datetime(int(t.split('-')[0]), int(t.split('-')[1]), int(t.split('T')[0].split('-')[2]), int(t.split('T')[1].split(':')[0]), int(t.split(':')[1]), int(t.split(':')[2].split('.')[0]))
-            dateTime -= hours_added
+            t = item.get('createDate')
+            if(t is not None): dateTime = " ".join(t.split('T'))
+            else: dateTime = ""
+            #dateTime = datetime.datetime(int(t.split('-')[0]), int(t.split('-')[1]), int(t.split('T')[0].split('-')[2]), int(t.split('T')[1].split(':')[0]), int(t.split(':')[1]), int(t.split(':')[2]))
+            #hours_added = datetime.timedelta(hours = int(t.split("+")[1].split(":")[0]) - 6, minutes = int(t.split("+")[1].split(":")[1]))
+            #dateTime -= hours_added
             xml += "<n:v>"
-            xml += "<n:id>" + str(item['id']) + "</n:id>"
-            xml += "<n:stateName>" + str(item['stateId']) + "</n:stateName>"
-            xml += "<n:senderStockName>" + str(item['senderStockId']) + "</n:senderStockName>"
-            xml += "<n:stateUpdateDate>" + str(item['createDate']) + "</n:stateUpdateDate>"
-            xml += "<n:stateName_and_updateDate>" + str(item['stateId']) + ", " + str(dateTime) + "</n:stateName_and_updateDate>"
+            xml += "<n:id>" + ifnull(item['id']) + "</n:id>"
+            xml += "<n:stateName>" + ifnull(item['stateName']) + "</n:stateName>"
+            xml += "<n:senderStockName>" + ifnull(item['senderStockName']) + "</n:senderStockName>"
+            xml += "<n:stateUpdateDate>" + ifnull(item['createDate']) + "</n:stateUpdateDate>"
+            xml += "<n:stateName_and_updateDate>" + ifnull(item['stateName']) + ", " + str(dateTime) + "</n:stateName_and_updateDate>"
             xml += "</n:v>"
         xml += "</n:GetListResponse>"
         xml += "</s:Body>"
@@ -148,6 +157,11 @@ def getRequestGet(self, id):
 
         json = r.json()
 
+        if(json is None or 'materialMovementRequests' not in json):
+            return generateErrorXml(self, "От сервера СУДМ 10.8.4.244:8010 на вызов GET /api/material-movement/v1.0/stock получен неожиданный ответ:", True, r.status_code)
+        elif(json['materialMovementRequests'] == []):
+            return generateErrorXml(self, "От сервера СУДМ 10.8.4.244:8010 на вызов GET-метода /api/material-movement/v1.0/stock получен пустой список materialMovementRequests",False, 0)
+
         xml = "<s:Envelope xmlns:s=\"http://schemas.xmlsoap.org/soap/envelope/\">"
         xml += "xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\""
         xml += "xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\">"
@@ -156,23 +170,23 @@ def getRequestGet(self, id):
         xml += "xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\""
         xml += "xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\">"
         for item in json['materialMovementRequests']:
-            xml += "<n:id>" + str(item['id']) + "</n:id>"
-            xml += "<n:stateId>" + str(item['stateId']) + "</n:stateId>"
-            xml += "<n:stateName>" + str(item['stateId']) + "</n:stateName>"
-            xml += "<n:senderStockId>" + str(item['senderStockId']) + "</n:senderStockId>"
-            xml += "<n:senderStockName>" + str(item['senderStockId']) + "</n:senderStockName>"
-            xml += "<n:receiverStockId>" + str(item['receiverStockId']) + "</n:receiverStockId>"
-            xml += "<n:createDate>" + str(item['createDate']) + "</n:createDate>"
-            xml += "<n:stateUpdateDate>" + str(item['createDate']) + "</n:stateUpdateDate>"
-            xml += "<n:rejectReasonId>" + str(item['rejectReasonId']) + "</n:rejectReasonId>"
-            xml += "<n:rejectReasonName>" + str(item['rejectReasonId']) + "</n:rejectReasonName>"
+            xml += "<n:id>" + ifnull(item['id']) + "</n:id>"
+            xml += "<n:stateId>" + ifnull(item['stateId']) + "</n:stateId>"
+            xml += "<n:stateName>" + ifnull(item['stateId']) + "</n:stateName>"
+            xml += "<n:senderStockId>" + ifnull(item['senderStockId']) + "</n:senderStockId>"
+            xml += "<n:senderStockName>" + ifnull(item['senderStockId']) + "</n:senderStockName>"
+            xml += "<n:receiverStockId>" + ifnull(item['receiverStockId']) + "</n:receiverStockId>"
+            xml += "<n:createDate>" + ifnull(item['createDate']) + "</n:createDate>"
+            xml += "<n:stateUpdateDate>" + ifnull(item['createDate']) + "</n:stateUpdateDate>"
+            xml += "<n:rejectReasonId>" + ifnull(item['rejectReasonId']) + "</n:rejectReasonId>"
+            xml += "<n:rejectReasonName>" + ifnull(item['rejectReasonId']) + "</n:rejectReasonName>"
             xml += "<n:note>" + ifnull(item['note']) + "</n:note>"
             for material in item['materials']:
                 xml += "<n:materials>"
-                xml += "<material_id>" + str(material['materialId']) + "</material_id>"
+                xml += "<material_id>" + ifnull(material['materialId']) + "</material_id>"
                 xml += "<material_name>" + ifnull(material['materialName']) + "</material_name>"
-                xml += "<unit_name>" +  ifnull(material['materialExternalUnitCode']) + "</unit_name>"
-                xml += "<volume>" + str(material['volume']) + "</volume>"
+                xml += "<unit_name>" + ifnull(material['materialExternalUnitCode']) + "</unit_name>"
+                xml += "<volume>" + ifnull(material['volume']) + "</volume>"
                 xml += "</n:materials>"
         xml += "</n:GetResponse>"
         xml += "</s:Body>"
